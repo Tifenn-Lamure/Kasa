2fa239d99ed94308b05d7919723cda953b541d47
Tifenn-Lamure <tifenn.lamure@hotmail.com>
2024-02-15 18:14:57 +0100

    Style du header avec CSS in JS

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,17 +1,43 @@
-import "./header.scss";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 import LogoKasa from "../../assets/logo_kasa.png";
 
+const HeaderDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem;
+`
+
+const Img = styled.img`
+  height: 40px;
+`
+
+const Nav = styled.nav`
+  display: flex;
+  align-items: space-between;
+  gap: 2rem;
+`
+
+const LinkNav = styled(Link)`
+  text-decoration: none;
+  color: black;
+  font-weight: 500;
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
 function Header() {
   return (
-    <header>
-      <img src={LogoKasa} alt="Logo de Kasa" />
-      <nav>
-        <Link to="/" className="pageLink">Accueil</Link>
-        <Link to="/about" className="pageLink">A Propos</Link>
-      </nav>
-    </header>
+    <HeaderDiv>
+      <Img src={LogoKasa} alt="Logo de Kasa" />
+      <Nav>
+        <LinkNav to="/" className="pageLink">Accueil</LinkNav>
+        <LinkNav to="/about" className="pageLink">A Propos</LinkNav>
+      </Nav>
+    </HeaderDiv>
   );
 }
 
