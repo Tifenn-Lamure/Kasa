e621a33ecbfaf82c900833e9a37ebc6fd120ca40
Tifenn-Lamure <tifenn.lamure@hotmail.com>
2024-02-22 00:06:59 +0100

    Gestion d'un id de propriété inexistant

--- src/pages/Rent/index.jsx
+++ src/pages/Rent/index.jsx
@@ -2,6 +2,9 @@ import styled from "styled-components";
 import { Fragment } from "react";
 import { useParams } from "react-router";
 import Slideshow from "../../components/Slideshow";
+import NotFound404 from '../../pages/NotFound404'
+// import { Navigate } from "react-router-dom";
+
 
 import logements from "../../data/logements";
 import colors from "../../utils/styles/colors";
@@ -17,14 +20,21 @@ function Rent() {
     const params = useParams();
     const rentId = params.id;
     const foundLogement = logements.find(lgt => lgt.id === rentId)
-    console.log('foundLogement', foundLogement)
 
-    return(
-        <Fragment>
-            <Slideshow pictures={foundLogement.pictures} />
-            <RentTitle>{foundLogement.title}</RentTitle>
-        </Fragment>
-    )
+    if(foundLogement) {
+        return(
+            <Fragment>
+                <Slideshow pictures={foundLogement.pictures} />
+                <RentTitle>{foundLogement.title}</RentTitle>
+            </Fragment>
+        )
+    }else{
+        return(
+            <NotFound404/>
+            // <Navigate to="/error404" replace={true} />
+        )
+    }
+    
 }
 
 export default Rent;
